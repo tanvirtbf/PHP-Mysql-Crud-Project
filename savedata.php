21ea2d51181f9c7e2cca6a347d92b1674de63a8a
tanvirtbf <tanver018765@gmail.com>
2024-07-13 11:10:36 +0600

    savedata sql complete

--- savedata.php
+++ savedata.php
@@ -1,8 +1,14 @@
 <?php 
 
-  $stu_name = $_POST['sname'];
-  $stu_address = $_POST['saddress'];
-  $stu_class = $_POST['class'];
-  $stu_phone = $_POST['sphone'];
+  echo $stu_name = $_POST['sname'];
+  echo $stu_address = $_POST['saddress'];
+  echo $stu_class = $_POST['class'];
+  echo $stu_phone = $_POST['sphone'];
+
+  $conn = mysqli_connect("localhost", "root", "", "crud") or die("Connection Failed");
+  $sql = "INSERT INTO student(sname,saddress,sclass,sphone) VALUES ('{$stu_name}','{$stu_address}','{$stu_class}','{$stu_phone}')";
+  $result = mysqli_query($conn, $sql) or die("Query Unsuccessful");
+
+  
 
 ?>
