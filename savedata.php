1fcb343594a51c968409486f36554a78e31b8849
tanvirtbf <tanver018765@gmail.com>
2024-07-13 11:18:34 +0600

    Add data code complete

--- savedata.php
+++ savedata.php
@@ -1,9 +1,9 @@
 <?php 
 
-  echo $stu_name = $_POST['sname'];
-  echo $stu_address = $_POST['saddress'];
-  echo $stu_class = $_POST['class'];
-  echo $stu_phone = $_POST['sphone'];
+  $stu_name = $_POST['sname'];
+  $stu_address = $_POST['saddress'];
+  $stu_class = $_POST['class'];
+  $stu_phone = $_POST['sphone'];
 
   $conn = mysqli_connect("localhost", "root", "", "crud") or die("Connection Failed");
   $sql = "INSERT INTO student(sname,saddress,sclass,sphone) VALUES ('{$stu_name}','{$stu_address}','{$stu_class}','{$stu_phone}')";
