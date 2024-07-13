56e7afc6d85c4bcab360e9057b4b7251d210bb0e
tanvirtbf <tanver018765@gmail.com>
2024-07-13 11:42:27 +0600

    query string id collect

--- edit.php
+++ edit.php
@@ -3,37 +3,42 @@
 <div id="main-content">
     <h2>Update Record</h2>
     <?php
-        $conn = mysqli_connect("localhost", "root", "", "crud") or die("Connection Failed");
-        $sql = "SELECT * FROM student ";
-        $result = mysqli_query($conn, $sql) or die("Query Unsuccessful");
+    $conn = mysqli_connect("localhost", "root", "", "crud") or die("Connection Failed");
+    $stu_id = $_GET['id'];
+    $sql = "SELECT * FROM student WHERE sid = {$stu_id}";
+    $result = mysqli_query($conn, $sql) or die("Query Unsuccessful");
 
-        if(mysqli_num_rows($result) > 0)
+    if (mysqli_num_rows($result) > 0) {
+
     ?>
-    <form class="post-form" action="updatedata.php" method="post">
-        <div class="form-group">
-            <label>Name</label>
-            <input type="hidden" name="sid" value="" />
-            <input type="text" name="sname" value="" />
-        </div>
-        <div class="form-group">
-            <label>Address</label>
-            <input type="text" name="saddress" value="" />
-        </div>
-        <div class="form-group">
-            <label>Class</label>
-            <select name="sclass">
-                <option value="" selected disabled>Select Class</option>
-                <option value="1">BCA</option>
-                <option value="2">BSC</option>
-                <option value="3">B.TECH</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label>Phone</label>
-            <input type="text" name="sphone" value="" />
-        </div>
-        <input class="submit" type="submit" value="Update" />
-    </form>
+        <form class="post-form" action="updatedata.php" method="post">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="hidden" name="sid" value="" />
+                <input type="text" name="sname" value="" />
+            </div>
+            <div class="form-group">
+                <label>Address</label>
+                <input type="text" name="saddress" value="" />
+            </div>
+            <div class="form-group">
+                <label>Class</label>
+                <select name="sclass">
+                    <option value="" selected disabled>Select Class</option>
+                    <option value="1">BCA</option>
+                    <option value="2">BSC</option>
+                    <option value="3">B.TECH</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label>Phone</label>
+                <input type="text" name="sphone" value="" />
+            </div>
+            <input class="submit" type="submit" value="Update" />
+        </form>
+    <?php  } else{
+        echo "Not Found Record";
+    } ?>
 </div>
 </div>
 </body>
