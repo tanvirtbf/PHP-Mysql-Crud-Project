f06e7bc0cfb383d4f5374bc4dacb6cd8b4f24771
tanvirtbf <tanver018765@gmail.com>
2024-07-17 02:47:43 +0600

    Associative Array and print_r function use for display

--- array.php
+++ array.php
@@ -4,21 +4,43 @@
 // 1. [] 
 // 2. array()
 
-$arr1 = ['Tanvir','Sadia','Maymuna'];
-$arr2 = array('Humaira','Ayesha','Khadija');
+// $arr1 = ['Tanvir','Sadia','Maymuna'];
+// $arr2 = array('Humaira','Ayesha','Khadija');
 
 // echo $arr1[2];
 // echo $arr2[1];
 
 
 //For Loop with Array 
-for($i=0;$i<count($arr1);$i++){
-  echo $arr1[$i];
-}
+// for($i=0;$i<count($arr1);$i++){
+//   echo $arr1[$i];
+// }
 
 //Foreach Loop with Array
-foreach($arr2 as $item){
-  echo $item;
-}
+// foreach($arr2 as $item){
+//   echo $item;
+// }
+
+$assArr = [
+  'Name' => 'Tanvir Ahmed',
+  'Wife' => 'Sadia Ahmed',
+  'Age' => 15,
+  'Class' => 'BSC 3rd year',
+  'Address' => 'Dhaka Sonir Akhra'
+];
+print_r($assArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
