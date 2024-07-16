1db309473b1bcb0956baeeddeb01f6f0c8c91bb7
tanvirtbf <tanver018765@gmail.com>
2024-07-17 02:33:40 +0600

    for loop with array

--- array.php
+++ array.php
@@ -4,11 +4,16 @@
 // 1. [] 
 // 2. array()
 
-$arr1 = [1,2,3,4,5];
-$arr2 = array('Tanvir','Sabbir','Rifat','Riduan','Riyad');
+$arr1 = ['Tanvir','Sadia','Maymuna'];
+$arr2 = array('Humaira','Ayesha','Khadija');
 
-echo $arr1[3];
-echo $arr2[1];
+// echo $arr1[2];
+// echo $arr2[1];
 
+
+//For Loop with Array 
+for($i=0;$i<count($arr1);$i++){
+  echo $arr1[$i];
+}
 
 ?>
